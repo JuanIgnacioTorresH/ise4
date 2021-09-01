559aff017a4b54394f7b9d4f1357d0f887252a82
JuanIgnacioTorresH <81965096+JuanIgnacioTorresH@users.noreply.github.com>
2021-08-31 21:24:40 -0300

    Add files via upload

--- cifrado.c
+++ cifrado.c
@@ -11,7 +11,29 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    // TODO
+    int i= 0;
+    int len= strlen (argv[1])+1;
+    if (strlen (argv[1]) != 26 ) {
+        printf ("Error: key debe tener 26 caracteres");
+        return 1;
+    }
+    while (argv[1][i]!='\0') {
+        if ( !isalpha (argv[1][i]) ) {
+            printf ("Error: key solo puede tener letras");
+            return 1;
+        }
+        i++;
+    }
+    i= 0;
+    while (argv[1][i]!='\0'){
+        for (int a=0; a<len; a+1){
+            for (int q= a+1; q<len; q++){
+                printf("Error: caracteres de la key no se pueden repetir");
+                return 1;
+            } 
+        }
+        i++;
+    }
 
     return 0;
 }
